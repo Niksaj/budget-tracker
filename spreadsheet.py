26bd3dfee0893e471b858525afb8c6c1698f66e0
Niksaj <nik@thejackos.net>
2022-12-26 23:02:59 +1030

    Excel document formatting

--- spreadsheet.py
+++ spreadsheet.py
@@ -3,23 +3,31 @@ from datetime import datetime
 import xlsxwriter
 import os
 
+def setup_output() :
+	if not os.path.exists(EXPORT_DIRECTORY) :
+		os.makedirs(EXPORT_DIRECTORY)
+
 def create_chart(workbook, worksheet, sheetName) :
 	chart = workbook.add_chart({"type": "line"})
 	chart.add_series({"values": "='{}'!A:A".format(sheetName)})
 	worksheet.insert_chart("F1", chart)
 
 def write_accounts(accounts) :
-	if not os.path.exists(EXPORT_DIRECTORY) :
-		os.makedirs(EXPORT_DIRECTORY)
-	
-	now = datetime.now();
+	setup_output()
+
+	now = datetime.now()
 	workbook = xlsxwriter.Workbook("{}Account Transactions_{}.xlsx".format(EXPORT_DIRECTORY, now.strftime(DATE_TIME_FORMAT)))
+	
+	# setup formats
+	money = workbook.add_format({"num_format": "$#,##0"})
+	dateFormat = workbook.add_format({"num_format":"mm/dd/yyyy"})
+
 	for account in accounts :
-		worksheetName = "{} - {}".format(account.name, account.balance);
+		worksheetName = "{} - {}".format(account.name, account.balance)
 		worksheet = workbook.add_worksheet(worksheetName)
 		for i, transaction in enumerate(account.transactions) :
-			worksheet.write(i, 0, float(transaction.amount))
-			worksheet.write(i, 1, transaction.created_at)
+			worksheet.write_number(i, 0, float(transaction.amount), money)
+			worksheet.write_datetime(i, 1, datetime.strptime(str.split(transaction.created_at, "+")[0], "%Y-%m-%dT%H:%M:%S"), dateFormat)
 			worksheet.write(i, 2, transaction.status)
 			worksheet.write(i, 3, transaction.message)
 		create_chart(workbook, worksheet, worksheetName)
